0fe09aa7b73132779e7a55313f6d4e98ee838361
Diogo Soares <soaresdiogodns@gmail.com>
2024-02-23 21:30:33 -0300

    Teste de insert com o aditivo nutritivo, sabor, tipo embalagem, tipo picole, ingrediente, conservante e revendedor

--- insert_main.py
+++ insert_main.py
@@ -1,5 +1,11 @@
 from conf.db_session import create_session
 from models.aditivo_nutritivo import AditivoNutritivo
+from models.sabor import Sabor
+from models.tipo_embalagem import TipoEmbalagem
+from models.tipo_picole import TipoPicole
+from models.ingrediente import Ingrediente
+from models.conservante import Conservante
+from models.revendedor import Revendedor
 
 
 # 1. Aditivo Nutritivo
@@ -9,22 +15,136 @@ def insert_aditivo_nutritivo() -> None:
     nome: str = input('Informe o nome do Aditivo Nutritivo: ')
     formula_quimica: str = input('Informe a fórmula química do aditivo: ')
     
-    an: AditivoNutritivo = AditivoNutritivo(nome=nome, formula_quimica=formula_quimica)
+    aditivo_nutritivo: AditivoNutritivo = AditivoNutritivo(nome=nome, formula_quimica=formula_quimica)
     
     with create_session() as session:  # Criando um contexto na variável session
         # Se eu não estivesse em uma sessão, seria necessário fechar a sessão com session.close()
-        session.add(an)  # Adicionando o objeto AditivoNutritivo
+        session.add(aditivo_nutritivo)  # Adicionando o objeto AditivoNutritivo
         
         # Commita para o banco de dados todas as operações que ocorreram dentro da sessão de uma vez só
         session.commit() # O commit é realizado em batch contendo todas as operações que foram realizadas dentro do contexto
 
     print('Aditivo Nutritivo cadastrado com sucesso')
-    print(f'ID: {an.id}')
-    print(f'Data de criação: {an.data_criacao}')
-    print(f'Nome: {an.nome}')
-    print(f'Fórmula química: {an.formula_quimica}')
+
+ 
+# 2. Sabor
+def insert_sabor() -> None:
+    print('Cadastro Sabor')
+    
+    nome: str = input('Informe o nome do Sabor: ')
+    
+    sabor: Sabor = Sabor(nome=nome)
+    
+    with create_session() as session:  # Criando um contexto na variável session
+        # Se eu não estivesse em uma sessão, seria necessário fechar a sessão com session.close()
+        session.add(sabor)  # Adicionando o objeto Sabor
+        
+        # Commita para o banco de dados todas as operações que ocorreram dentro da sessão de uma vez só
+        session.commit() # O commit é realizado em batch contendo todas as operações que foram realizadas dentro do contexto
+
+    print('Sabor cadastrado com sucesso')
+
+
+# 3. Tipo Embalagem
+def insert_tipo_embalagem() -> None:
+    print('Cadastro Tipo Embalagem')
+    
+    nome: str = input('Informe o nome do Tipo da embalagem: ')
+    
+    tipo_embalagem: TipoEmbalagem = TipoEmbalagem(nome=nome)
+    
+    with create_session() as session:  # Criando um contexto na variável session
+        # Se eu não estivesse em uma sessão, seria necessário fechar a sessão com session.close()
+        session.add(tipo_embalagem)  # Adicionando o objeto Sabor
+        
+        # Commita para o banco de dados todas as operações que ocorreram dentro da sessão de uma vez só
+        session.commit() # O commit é realizado em batch contendo todas as operações que foram realizadas dentro do contexto
+    
+    print('Tipo Embalagem cadastrado com sucesso')
+
+
+
+# 4. Tipo Picole
+def insert_tipo_picole() -> None:
+    print('Cadastro Tipo Picole')
+    
+    nome: str = input('Informe o nome do Tipo Picole: ')
+    
+    tipo_picole: TipoPicole = TipoPicole(nome=nome)
+    
+    with create_session() as session:  # Criando um contexto na variável session
+        # Se eu não estivesse em uma sessão, seria necessário fechar a sessão com session.close()
+        session.add(tipo_picole)  # Adicionando o objeto Sabor
+        
+        # Commita para o banco de dados todas as operações que ocorreram dentro da sessão de uma vez só
+        session.commit() # O commit é realizado em batch contendo todas as operações que foram realizadas dentro do contexto
+
+    print('Tipo Picole cadastrado com sucesso')
+
+
+# 5. Ingrediente
+def insert_ingrediente() -> None:
+    print('Cadastro Ingrediente')
+    
+    nome: str = input('Informe o nome do Ingrediente: ')
+    
+    ingrediente: Ingrediente = Ingrediente(nome=nome)
+    
+    with create_session() as session:  # Criando um contexto na variável session
+        # Se eu não estivesse em uma sessão, seria necessário fechar a sessão com session.close()
+        session.add(ingrediente)  # Adicionando o objeto Sabor
+        
+        # Commita para o banco de dados todas as operações que ocorreram dentro da sessão de uma vez só
+        session.commit() # O commit é realizado em batch contendo todas as operações que foram realizadas dentro do contexto
+
+    print('Ingrediente cadastrado com sucesso')
+    
+    
+# 5. Conservante
+def insert_conservante() -> None:
+    print('Cadastro Conservante')
+    
+    nome: str = input('Informe o nome do Conservante: ')
+    descricao: str = input('Informe a descrição do Conservante: ')
+    
+    conservante: Conservante = Conservante(nome=nome, descricao=descricao)
+    
+    with create_session() as session:  # Criando um contexto na variável session
+        # Se eu não estivesse em uma sessão, seria necessário fechar a sessão com session.close()
+        session.add(conservante)  # Adicionando o objeto Sabor
+        
+        # Commita para o banco de dados todas as operações que ocorreram dentro da sessão de uma vez só
+        session.commit() # O commit é realizado em batch contendo todas as operações que foram realizadas dentro do contexto
+
+    print('Conservante cadastrado com sucesso')
+
+
+# 5. Revendedor
+def insert_revendedor() -> None:
+    print('Cadastro Revendedor')
+    
+    cnpj: str = input('Informe o cnpj do Revendedor: ')
+    razao_social: str = input('Informe a razão social do Revendedor: ')
+    contato: str = input('Informe o contato do Revendedor: ')
+    
+    revendedor: Revendedor = Revendedor(cnpj=cnpj, razao_social=razao_social, contato=contato)
+    
+    with create_session() as session:  # Criando um contexto na variável session
+        # Se eu não estivesse em uma sessão, seria necessário fechar a sessão com session.close()
+        session.add(revendedor)  # Adicionando o objeto Sabor
+        
+        # Commita para o banco de dados todas as operações que ocorreram dentro da sessão de uma vez só
+        session.commit() # O commit é realizado em batch contendo todas as operações que foram realizadas dentro do contexto
+
+    print('Revendedor cadastrado com sucesso')
 
 
 
 if __name__ == '__main__':
     insert_aditivo_nutritivo()
+    insert_sabor()
+    insert_tipo_embalagem()
+    insert_tipo_picole()
+    insert_ingrediente()
+    insert_conservante()
+    insert_revendedor()
--- models/tipo_embalagem.py
+++ models/tipo_embalagem.py
@@ -7,6 +7,7 @@ class TipoEmbalagem(ModelBase):  # Herdando modelo base
     
     id: int = sa.Column(sa.BigInteger, primary_key=True, autoincrement=True) # Chave primária do model (tabela). Passado de forma automática
     data_criacao: datetime = sa.Column(sa.DateTime, default=datetime.now, index=True) # Valor padrão é a hora exata em que o objeto foi criado
+    nome: str = sa.Column(sa.String(45), unique=True, nullable=False) # Único e não nulo com limite de até 45 caracteres
 
 
     def __repr__(self) -> str:  # Método usado para definir a representação "oficial" de um objeto
