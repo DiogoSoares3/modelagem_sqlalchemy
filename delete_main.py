d10351f82fcdc7c0b2fec05f45d7bb8779d8a113
Diogo Soares <soaresdiogodns@gmail.com>
2024-02-27 21:13:54 -0300

    Funções para exclusao e configuração de cascate

---
+++ delete_main.py
@@ -0,0 +1,46 @@
+"""
+1. Buscar o registro a ser atualizado
+2. Fazer a exclusão no registro
+3. Salvar o registro no banco de dados
+"""
+from conf.db_session import create_session
+from models.revendedor import Revendedor
+from models.picole import Picole
+from typing import Optional
+
+
+def deletar_picole(id_picole: int) -> None:
+    with create_session() as session:
+        # Passo 1:
+        picole: Optional[Picole] = session.query(Picole).filter(Picole.id == id_picole).one_or_none()
+        
+        if picole:
+            # Passo 2:
+            session.delete(picole)
+            # Passo 3:
+            session.commit()
+            print('====================')
+            print(picole)
+            print('====================')
+        else:
+            print(f'Não encontrei picole com id {id_picole}')
+            
+            
+def deletar_revendedor(id_revendedor: int) -> None:
+    with create_session() as session:
+        # Passo 1:
+        revendedor: Revendedor = session.query(Revendedor).filter(Revendedor.id == id_revendedor).one_or_none()
+        
+        if revendedor:
+            # Passo 2:
+            session.delete(revendedor)
+            # Passo 3:
+            session.commit()
+        else:
+            print(f'Não encontrei revendedor com id {id_revendedor}')
+
+
+
+if __name__ == '__main__':
+    #deletar_picole(id_picole=1)
+    deletar_revendedor(id_revendedor=2) # Erro pois o revendedor com id 2 tem referência na tabela notas_fiscais (impedimento)
--- models/nota_fiscal.py
+++ models/nota_fiscal.py
@@ -10,24 +10,24 @@ from models.lote import Lote
 # Nota Fiscal pode ter vários Lotes (tabela de relacionamento muitos para muitos)
 lotes_nota_fiscal = sa.Table(  # Criando a tabela de muitos para muitos
     'lotes_nota_fiscal',  # Nome da tabela
-    ModelBase.metadata,  # 
+    ModelBase.metadata, 
     sa.Column('id_nota_fiscal', sa.BigInteger, sa.ForeignKey('notas_fiscais.id')),  # Chave estrangeira
     sa.Column('id_lote', sa.BigInteger, sa.ForeignKey('lotes.id')),  # Chave estrangeira
     
 )
 
 
-class NotaFiscal(ModelBase):  # Herdando modelo base
-    __tablename__: str = 'notas_fiscais' # Nome da tabela no banco de dados no plural
+class NotaFiscal(ModelBase):  
+    __tablename__: str = 'notas_fiscais' 
     
-    id: int = sa.Column(sa.BigInteger, primary_key=True, autoincrement=True) # Chave primária do model (tabela). Passado de forma automática
-    data_criacao: datetime = sa.Column(sa.DateTime, default=datetime.now, index=True) # Valor padrão é a hora exata em que o objeto foi criado
-    valor: float = sa.Column(sa.DECIMAL(8,2), nullable=False) # Valor decimal com até 8 dígitos e 2 dígitos após a vírgula
+    id: int = sa.Column(sa.BigInteger, primary_key=True, autoincrement=True) 
+    data_criacao: datetime = sa.Column(sa.DateTime, default=datetime.now, index=True) 
+    valor: float = sa.Column(sa.DECIMAL(8,2), nullable=False) 
     numero_serie: str = sa.Column(sa.String(45), unique=True, nullable=False)
     descricao: str = sa.Column(sa.String(200), nullable=False)
 
-    id_revendedor: int = sa.Column(sa.BigInteger, sa.ForeignKey('revendedores.id')) # Chave estrangeira --> tabela.coluna 
-    revendedor: Revendedor = orm.relationship('Revendedor', lazy='joined') # Configuração interna do sqlalchemy necessária quando tem chave estrangeira
+    id_revendedor: int = sa.Column(sa.BigInteger, sa.ForeignKey('revendedores.id', ondelete='CASCADE'))  # Efeito cascata quando ocorrer a exclusao de um revendedor ocorrer (default é impedimento)
+    revendedor: Revendedor = orm.relationship('Revendedor', lazy='joined', cascade='delete')  # Vai realizar o join para obter o relacionamento e vai deletar o registro todo quando houver cascade
     
     # Uma nota Fiscal pode ter vários lotes e um lote está ligado a uma nota fiscal
     lotes: List[Lote] = orm.relationship('Lote', secondary=lotes_nota_fiscal, backref='lote', lazy='dynamic')
